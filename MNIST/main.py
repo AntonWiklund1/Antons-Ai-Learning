0e092578739ae3a37aa4736f0a85753fc803c6dd
AntonWiklund1 <awiklund76@gmail.com>
2024-02-16 15:27:23 +0200

    Add MNIST project files and initial code

--- MNIST/main.py
+++ MNIST/main.py
@@ -11,7 +11,6 @@ model = tf.keras.Sequential([
     layers.Dense(10, activation='softmax') # One neuron for each class 0,1,2,3,...,9
 ])
 
-
 # Compile the model. 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',  # Use sparse_categorical_crossentropy
@@ -28,14 +27,11 @@ x_test = x_test.reshape(-1, 784)  # Reshape from (10000, 28, 28) to (10000, 784)
 # Normalize the images
 x_train, x_test = x_train / 255.0, x_test / 255.0  # Normalize the data
 
-
 # Define the early stopping callback
 early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-
 
 # Train the model
 model.fit(x_train, y_train, epochs=50, validation_split=0.1, callbacks=[early_stopping])
 
 # Evaluate the model
 model.evaluate(x_test, y_test)
-
